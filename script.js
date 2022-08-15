21556534545ab00307c02c3a6622c4932e460126
ryenut <erikamaegalo@gmail.com>
2022-08-16 04:44:11 +0800

    create function to return a random string

---
+++ script.js
@@ -0,0 +1,9 @@
+// Test function to return a random string
+
+const compMove = ['Rock', 'Paper', 'Scissors'];
+        
+function getComputerChoice () {
+    return randomMove = compMove[Math.floor(Math.random() * compMove.length)]
+}
+
+getComputerChoice();
