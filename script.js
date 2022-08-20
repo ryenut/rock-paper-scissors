bae4c121ba133d76d3f0a054df5d7a662284e2a7
ryenut <erikamaegalo@gmail.com>
2022-08-20 15:12:50 +0800

    Test a single round of the game (rock)

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 // Test function to return a random string
+// 
 
-const compMove = ['Rock', 'Paper', 'Scissors'];
+        const compMove = ['rock', 'paper', 'scissors'];
+                
+        function getComputerChoice() {
+            return randomMove = compMove[Math.floor(Math.random() * compMove.length)]
+        }
+
+        const computerSelection = getComputerChoice(); 
+        const playerSelection = 'rock';
         
-function getComputerChoice () {
-    return randomMove = compMove[Math.floor(Math.random() * compMove.length)]
-}
+        function singleRound (playerSelection, computerSelection) {           
+            if (playerSelection === 'rock' && computerSelection === 'rock') {
+                return 'It\'s a tie!';
+            
+            } else if (playerSelection === 'rock' && computerSelection === 'paper'){
+                return 'You lose! Paper beats rock.';
+            
+            } else if (playerSelection === 'rock' && computerSelection === 'scissors'){
+                return 'You win!';
+        }
+    }
+
+        console.log(singleRound(playerSelection, computerSelection));
