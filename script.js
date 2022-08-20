ff481281342e2ef6330155a26d927628b88eb5c7
ryenut <erikamaegalo@gmail.com>
2022-08-20 18:19:22 +0800

    Complete a single playRound function

--- script.js
+++ script.js
@@ -12,15 +12,37 @@
         const playerSelection = answer;
         
         function singleRound (playerSelection, computerSelection) {           
-            if (playerSelection === 'rock' && computerSelection === 'rock') {
+            if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock') {
                 return 'It\'s a tie!';
             
-            } else if (playerSelection === 'rock' && computerSelection === 'paper'){
+            } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper'){
                 return 'You lose! Paper beats rock.';
             
-            } else if (playerSelection === 'rock' && computerSelection === 'scissors'){
+            } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors'){
                 return 'You win!';
-        }
+            
+            } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'paper') {
+                return 'It\'s a tie!';
+            
+            } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') {
+                return 'You lose! Rock beats paper.'
+            
+            } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors') {
+                return 'You lose! Scissors beat paper.'
+            
+            } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'scissors') {
+                return 'It\'s a tie!'
+
+            } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock') {
+                return 'You lose! Rock beats scissors.'
+
+            } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') {
+                return 'You win!'
+            }   
     }
 
-        console.log(singleRound(playerSelection, computerSelection));
+        const playerScore
+        const computerScore
+
+        function game() {}
+        
